baae2851cb368e0249567369ec7be9ff017cb112
Luiz Barros <lck.barros@gmail.com>
2026-03-23 22:17:46 -0300

    first commit

---
+++ src/main/java/com/luizbarros/java_docker_carbon_footprint/JavaDockerCarbonFootprintApplication.java
@@ -0,0 +1,13 @@
+package com.luizbarros.java_docker_carbon_footprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDockerCarbonFootprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDockerCarbonFootprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizbarros/java_docker_carbon_footprint/JavaDockerCarbonFootprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizbarros.java_docker_carbon_footprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDockerCarbonFootprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
